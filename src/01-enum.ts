28941c79ee0685b2831ca2da2706053f62bb5597
htmtmx <cesar.pineda@daksha.mx>
2023-12-29 13:21:52 -0600

    03 - ➕ Add ts-node, typescript, and @capacitor/camera dependencies ✨ Add enum and camera-capacitor files

---
+++ src/01-enum.ts
@@ -0,0 +1,23 @@
+//@ts-check
+
+// Enum es un tipo de dato que permite definir un conjunto de constantes con nombre
+// Se puede acceder a los valores de un enum con la notación de punto
+// Los valores de un enum pueden ser números o cadenas de texto
+
+enum ROLES {
+  ADMIN = 'admin',
+  SELLER = 'seller',
+  CUSTOMER = 'customer',
+}
+
+type User = {
+  username: string;
+  role: ROLES;
+}
+
+const nicoUser: User = {
+  username: 'Nicolas',
+  role: ROLES.ADMIN
+}
+
+// Libreia para trabajar con enums, Capacitor
