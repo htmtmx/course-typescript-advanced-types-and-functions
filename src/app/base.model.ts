b8595160066bb8c035b852760e0978559eaf7a60
htmtmx <cesar.pineda@daksha.mx>
2024-01-02 13:30:21 -0600

    16 - ✨ Using readonly to protect id and createdAt

--- src/app/base.model.ts
+++ src/app/base.model.ts
@@ -1,5 +1,5 @@
 export interface Base {
-  id: string | number,
-  createdAt: Date,
+  readonly id: string | number,
+  readonly createdAt: Date,
   updatedAt: Date,
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
-    products.push(data);
+  data.id = 'dfsdfsdf';
+  data.createdAt = new Date(1998, 1, 1);
+  products.push(data);
 }
